a2c6042d00909a60ff2e972384241f183f90ff59
julia <juliaberezhnova98@gmail.com>
2016-10-16 16:36:46 +0300

    task 1 added

---
+++ BinarySearchTree/BinarySearchTree/IBinarySearchTree.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace MyPrivateBinarySearchTree
+{
+    /// <summary>
+    /// Interface for binary search tree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface IBinarySearchTree<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Insertation method
+        /// True if data is inserted successfully
+        /// False if there exists the same data in the tree
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        bool Insert(T data);
+
+        /// <summary>
+        /// True if tree contains needed data
+        /// False if it does not
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        bool Contains(T data);
+
+        /// <summary>
+        /// Remover
+        /// True if node with given data is removed successfully
+        /// False if operation is failed
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        bool Remove(T data);
+
+        /// <summary>
+        /// Prints values in ascending order
+        /// Using infix traverse
+        /// </summary>
+        void Print();
+    }
+}
